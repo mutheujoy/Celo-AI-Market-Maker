84767096b603c9e8a974423497196cfd557b16f6
Joy Mutheu <joywriter271@gmail.com>
2024-09-25 21:28:33 +0300

    Update

---
+++ Rewards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Rewards = () => {
+    return (
+        <div className="rewards">
+            <h2>Your Rewards</h2>
+            {/* Rewards Logic */}
+            <p>Earn cKES rewards by trading and providing liquidity!</p>
+        </div>
+    );
+};
+
+export default Rewards;
